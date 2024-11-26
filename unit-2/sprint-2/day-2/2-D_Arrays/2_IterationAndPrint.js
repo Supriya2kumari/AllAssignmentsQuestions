b420485ac4d5c77e72d3f45229ed133958aa8e8b
supriya kumari <supriyakumarisj@gmail.com>
2024-11-26 20:04:56 +0530

    Used for loop to iterate over 2-D array and print the elements

---
+++ unit-2/sprint-2/day-2/2-D_Arrays/2_IterationAndPrint.js
@@ -0,0 +1,12 @@
+let evenMatrix = [
+    [10, 8,],
+    [2, 6],
+    [4, 10],
+    [50, 20]
+];
+
+for(let i = 0; i < evenMatrix.length; i++){
+    for(let j = 0; j < evenMatrix[i].length; j++){
+        console.log(evenMatrix[i][j]);
+    }
+}
