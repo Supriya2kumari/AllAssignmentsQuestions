74ecc01bd902f1546b6cceff82dc562f1cdc0b69
supriya kumari <supriyakumarisj@gmail.com>
2024-12-03 19:29:31 +0530

    path changed

--- unit-2/sprint-2/day-3/Objects/10_SiniorDiscountEligibility.js
+++
@@ -1,7 +0,0 @@
-let isEligibleForDiscount = (age) => {
-    (age <= 0) ? console.log("Invalid Age") 
-    : (age >= 60) ? console.log(`Eligible for Sinior Discount`) 
-    : console.log(`Not Eligible for Sinior Discount`);
-}
-isEligibleForDiscount(65);
-isEligibleForDiscount(-1);
