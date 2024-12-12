104c4f1e80c841dab280d346c82fd6515519d3d1
supriya kumari <supriyakumarisj@gmail.com>
2024-12-12 22:09:44 +0530

    handle edge cases

--- unit-2/sprint-3/day-2/Closures/03_AccountBalance.js
+++ unit-2/sprint-3/day-2/Closures/03_AccountBalance.js
@@ -1,10 +1,20 @@
 function createBankAccount(balance){
     return {
         deposite: function(amount){
-            return balance += amount;
+            if(amount > 0){
+                return balance += amount;
+            }else{
+                return "balance should be greater than 0."
+            }
         },
         withdraw: function(amount) {
-            return balance -= amount;
+            if(amount > 0 && amount <= balance){
+                return balance -= amount;
+            } else if(amount > balance){
+                return "Insufficient balance.";
+            } else{
+                return "Amount should be greater than 0.";
+            }
         },
         getBalance: function(){
             return balance;
