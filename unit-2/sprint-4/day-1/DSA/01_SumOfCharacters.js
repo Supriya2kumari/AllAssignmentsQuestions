5506c7c0e0f57d7476c0c4136fbe97637081f623
supriya kumari <supriyakumarisj@gmail.com>
2024-12-12 22:10:14 +0530

    modified the code

--- unit-2/sprint-4/day-1/DSA/01_SumOfCharacters.js
+++ unit-2/sprint-4/day-1/DSA/01_SumOfCharacters.js
@@ -1,3 +1,10 @@
+/* print the sum of all the characters, "a" = 1 and all the characters
+   value is previous value  plus 1.
+   e.g. a = 1, b = 2, c = 3,........, z = 26
+   s = "abcd"
+   sum = 1 + 2 + 3 + 4 = 10
+*/
+
 function sumOfCharacters(s){
     let sum = 0;
     for(let i = 0; i < s.length; i++){
