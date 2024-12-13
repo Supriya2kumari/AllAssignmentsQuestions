f67f7ae1a92f337b3ac493052f69c68aee582fe9
supriya kumari <supriyakumarisj@gmail.com>
2024-12-13 22:05:13 +0530

    modified

--- unit-2/sprint-4/day-1/DSA/03_Dominating.js
+++ unit-2/sprint-4/day-1/DSA/03_Dominating.js
@@ -1,4 +1,13 @@
-function solve(N,arr){
+/*  check if array is dominating or not, if frequency of array element is more 
+    it is dominating, then print "YES", otherwise print "NO".
+    e.g.
+    1.)  n = 5, arr = [1, 2, 3, 2, 1]
+         output : NO
+    2.)  n = 5, arr = [1, 2, 3, 3, 3]
+         output : YES
+*/
+
+function checkDominatingArray(N,arr){
   let obj = {};
   for(let i = 0; i < N; i++){
     let num = arr[i];
@@ -26,3 +35,5 @@ function solve(N,arr){
     console.log("NO");
   }
 }
+checkDominatingArray(5, [1, 2, 3, 3, 3]);
+checkDominatingArray(5, [1, 2, 3, 2, 3]);
