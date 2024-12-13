63ec7ab11d84ff90a2c5cd98d421ac0d3d4a2b5a
supriya kumari <supriyakumarisj@gmail.com>
2024-12-13 22:04:41 +0530

    modified

--- unit-2/sprint-4/day-1/DSA/02_OddPrimeLoop.js
+++ unit-2/sprint-4/day-1/DSA/02_OddPrimeLoop.js
@@ -1,5 +1,11 @@
-function primeLoopCheck(num){
-	for(let i = 2; i <= num; i++){
+/* print all the odd prime numbers between 1 and given 'N',
+   1 should not included, print in different line
+	e.g. N = 10
+	output : 3 5 7
+*/
+
+function primeLoopCheck(N){
+	for(let i = 2; i <= N; i++){
 	  if(i % 2 === 1){
 	    let factor = 0;
 	    for(let j = 1; j <= i; j++){
@@ -13,3 +19,4 @@ function primeLoopCheck(num){
 	  }
 	}
 }
+primeLoopCheck(10);
